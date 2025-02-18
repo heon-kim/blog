af84d213cf6b197bdeb908f1cdf48359ed4e7db0
heon-kim <vndtka123@gmail.com>
2025-02-18 23:38:31 +0900

    FIX. 빌드 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,25 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-markdown',
+            'react-syntax-highlighter'
+          ],
+          markdown: [
+            'markdown-to-jsx',
+            'gray-matter',
+            'front-matter'
+          ]
+        }
+      }
+    }
+  }
 })
